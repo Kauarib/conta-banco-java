c7b3146dab90a55cb4318beefd8a1c9001c71eb4
Kauã Ribeiro <rkaua550@gmail.com>
2025-02-17 20:24:22 -0300

    melhorias do projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,36 +1,43 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try{
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Olá, bem-vindo ao Rbank!");
+            System.out.println("Olá, bem-vindo ao Rbank!");
 
-        System.out.print("Digite o número da sua Agência: ");
-        String agencia = scanner.next();
+            System.out.print("Digite o número da sua Agência: ");
+            String agencia = scanner.next();
 
-        System.out.print("Agora digite o número da sua conta: ");
-        int numeroConta = scanner.nextInt();
+            System.out.print("Agora digite o número da sua conta: ");
+            int numeroConta = scanner.nextInt();
 
-        scanner.nextLine(); // Consumir a quebra de linha pendente
+            scanner.nextLine(); // Consumir a quebra de linha pendente
 
-        System.out.print("Diga seu nome: ");
-        String nomeCliente = scanner.nextLine();
+            System.out.print("Diga seu nome: ");
+            String nomeCliente = scanner.nextLine();
 
-        System.out.print("Quanto deseja depositar? ");
-        double valorDeposito = scanner.nextDouble();
+            System.out.print("Quanto deseja depositar? ");
+            double valorDeposito = scanner.nextDouble();
 
-        System.out.println("\n---------------------------------");
-        System.out.println("Olá, " + nomeCliente + "!");
-        System.out.println("Seu depósito foi realizado com sucesso.");
-        System.out.println("Detalhes da conta:");
-        System.out.println("Agência: " + agencia);
-        System.out.println("Conta: " + numeroConta);
-        System.out.printf("Valor depositado: R$ %.2f%n", valorDeposito);
-        System.out.println("---------------------------------");
+            System.out.println("\n---------------------------------");
+            System.out.println("Olá, " + nomeCliente + "!");
+            System.out.println("Seu depósito foi realizado com sucesso.");
+            System.out.println("Detalhes da conta:");
+            System.out.println("Agência: " + agencia);
+            System.out.println("Conta: " + numeroConta);
+            System.out.printf("Valor depositado: R$ %.2f%n", valorDeposito);
+            System.out.println("---------------------------------");
 
-        scanner.close();
+
+            scanner.close();
+        }catch(InputMismatchException e){
+            
+            System.out.println("os campos conta e depósito devem ser valores numéricos.");
+        }
     }
 }
 
